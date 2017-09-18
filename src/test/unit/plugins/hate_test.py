9697fe277be5a7a8b186bd4624d1c9f0a2d827ee
Roman Scheidegger <roman.scheidegger@gmail.com>
2017-09-18 21:53:24 +0200

    retrofit ChatMessagestub

--- src/test/unit/plugins/hate_test.py
+++ src/test/unit/plugins/hate_test.py
@@ -1,5 +1,6 @@
 import unittest
 
+from src.test.unit.plugins.utils import ChatMessageStub
 from src.gabi.hate import ShitCommand, AidsCommand, SihCommand
 from src.gabi.core import ChatMessage
 
@@ -7,36 +8,30 @@ from src.gabi.core import ChatMessage
 class TestShitCommand(unittest.TestCase):
     def setUp(self):
         self.cmd = ShitCommand()
-        self.msg = ChatMessage("", "testuser", self.chat_reply)
-
-    def chat_reply(self, msg_text):
-        self.response = msg_text
+        self.msg = ChatMessageStub()
 
     def test_shit_command_replies_holy_shit(self):
         self.cmd.on_execute(self.msg)
 
-        self.assertEqual("Holy shit!", self.response)
+        self.assertEqual("Holy shit!", self.msg.response)
 
 
 class TestAidsCommand(unittest.TestCase):
     def setUp(self):
         self.cmd = AidsCommand()
-        self.msg = ChatMessage("", "test_user", self.chat_reply)
-
-    def chat_reply(self, msg_text):
-        self.response = msg_text
+        self.msg = ChatMessageStub()
 
     def test_aids_command_without_user(self):
         self.cmd.on_execute(self.msg)
 
-        self.assertEqual("test_user, who should get AIDS?", self.response)
+        self.assertEqual("test_user, who should get AIDS?", self.msg.response)
 
     def test_aids_command_with_user(self):
         self.msg.msg = "looser"
 
         self.cmd.on_execute(self.msg)
 
-        self.assertEqual("looser, get AIDS!", self.response)
+        self.assertEqual("looser, get AIDS!", self.msg.response)
 
 
 class TestSihCommand(unittest.TestCase):

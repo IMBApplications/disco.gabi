82f3f22b733c58418b2f7d56d8b9254802a5452b
Roman Scheidegger <roman.scheidegger@gmail.com>
2017-09-18 22:27:29 +0200

    unit test for roll command

--- src/test/unit/plugins/fun_test.py
+++ src/test/unit/plugins/fun_test.py
@@ -1,5 +1,9 @@
 import unittest
 
+from unittest.mock import patch
+
+from random import Random
+
 from src.gabi.fun import *
 from src.test.unit.plugins.utils import ChatMessageStub
 
@@ -20,6 +24,30 @@ class TestGroovyCommand(unittest.TestCase):
         self.cmd.on_execute(self.msg)
 
         self.assertEqual("Groovy? Did i hear groovy? looser is groovy!", self.msg.response)
+
+
+class TestRollCommand(unittest.TestCase):
+    def setUp(self):
+        self.random = Random(666)
+        self.cmd = RollCommand()
+        self.msg = ChatMessageStub()
+
+    @patch('src.gabi.fun.random')
+    def test_roll_command_without_argument(self, random):
+        random.randint._mock_side_effect = self.random.randint
+
+        self.cmd.on_execute(self.msg)
+
+        self.assertEqual("4", self.msg.response)
+
+    @patch('src.gabi.fun.random')
+    def test_roll_command_with_valid_argument(self, random):
+        random.randint._mock_side_effect = self.random.randint
+        self.msg.msg = "100"
+
+        self.cmd.on_execute(self.msg)
+
+        self.assertEqual("59", self.msg.response)
 
 
 class TestImbaCommand(unittest.TestCase):
